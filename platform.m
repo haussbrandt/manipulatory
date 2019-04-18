00b42e16ff5ea10d6981d36b54e05f3d78f1b7b0
Haussbrandt <haussbrandt@gmail.com>
2019-04-18 17:25:25 +0200

    Changed the equations to fix a bug. Closes #2

--- platform.m
+++ platform.m
@@ -1,26 +1,26 @@
 function p = platform(a, b, g, x0, y0, z0, dx, dy)
 x1 = dx + x0 + dy*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)) - dx*cosd(b)*cosd(g);
-y1 = dy + y0 - dy*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)) - dx*cosd(b)*cosd(g);
+y1 = dy + y0 - dy*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)) - dx*cosd(b)*sind(g);
 z1 = z0 + dx*sind(b) - dy*cosd(b)*sind(a);
 
 x2 = x0 - dx + dy*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)) + dx*cosd(b)*cosd(g);
-y2 = dy + y0 - dy*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)) + dx*cosd(b)*cosd(g);
+y2 = dy + y0 - dy*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)) + dx*cosd(b)*sind(g);
 z2 = z0 - dx*sind(b) - dy*cosd(b)*sind(a);
-
-x3 = dx - x0/2 + (3^(1/2)*y0)/2 - (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 - (3^(1/2)*cosd(b)*cosd(g))/2) + (dy/2 - (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 + (cosd(a)*sind(g))/2 - (cosd(g)*sind(a)*sind(b))/2);
-y3 = dy - y0/2 - (3^(1/2)*x0)/2 - (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 + (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 - (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 - (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
+ 
+x3 = dx - x0/2 + (3^(1/2)*y0)/2 - (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 - (3^(1/2)*cosd(b)*sind(g))/2) + (dy/2 - (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 + (cosd(a)*sind(g))/2 - (cosd(g)*sind(a)*sind(b))/2);
+y3 = dy - y0/2 - (3^(1/2)*x0)/2 - (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*sind(g))/2 + (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 - (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 - (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2); 
 z3 = z0 - sind(b)*(dx/2 + (3^(1/2)*dy)/2) + cosd(b)*sind(a)*(dy/2 - (3^(1/2)*dx)/2);
-
-x4 = (3^(1/2)*y0)/2 - x0/2 - dx + (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 - (3^(1/2)*cosd(b)*cosd(g))/2) + (dy/2 + (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 + (cosd(a)*sind(g))/2 - (cosd(g)*sind(a)*sind(b))/2);
-y4 = dy - y0/2 - (3^(1/2)*x0)/2 + (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 + (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 + (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 - (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
+ 
+x4 = (3^(1/2)*y0)/2 - x0/2 - dx + (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 - (3^(1/2)*cosd(b)*sind(g))/2) + (dy/2 + (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 + (cosd(a)*sind(g))/2 - (cosd(g)*sind(a)*sind(b))/2);
+y4 = dy - y0/2 - (3^(1/2)*x0)/2 + (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*sind(g))/2 + (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 + (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 - (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
 z4 = z0 + sind(b)*(dx/2 - (3^(1/2)*dy)/2) + cosd(b)*sind(a)*(dy/2 + (3^(1/2)*dx)/2);
-
-x5 = dx - x0/2 - (3^(1/2)*y0)/2 - (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 + (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 + (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 - (cosd(a)*sind(g))/2 + (cosd(g)*sind(a)*sind(b))/2);
-y5 = dy - y0/2 + (3^(1/2)*x0)/2 - (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 - (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 + (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 + (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
+ 
+x5 = dx - x0/2 - (3^(1/2)*y0)/2 - (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 + (3^(1/2)*cosd(b)*sind(g))/2) - (dy/2 + (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 - (cosd(a)*sind(g))/2 + (cosd(g)*sind(a)*sind(b))/2);
+y5 = dy - y0/2 + (3^(1/2)*x0)/2 - (dx/2 - (3^(1/2)*dy)/2)*((cosd(b)*sind(g))/2 - (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 + (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 + (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
 z5 = z0 - sind(b)*(dx/2 - (3^(1/2)*dy)/2) + cosd(b)*sind(a)*(dy/2 + (3^(1/2)*dx)/2);
-
-x6 = (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 + (3^(1/2)*cosd(b)*cosd(g))/2) - x0/2 - (3^(1/2)*y0)/2 - dx - (dy/2 - (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 - (cosd(a)*sind(g))/2 + (cosd(g)*sind(a)*sind(b))/2);
-y6 = dy - y0/2 + (3^(1/2)*x0)/2 + (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 - (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 - (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 + (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
+ 
+x6 = (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*cosd(g))/2 + (3^(1/2)*cosd(b)*sind(g))/2) - x0/2 - (3^(1/2)*y0)/2 - dx - (dy/2 - (3^(1/2)*dx)/2)*((3^(1/2)*(cosd(a)*cosd(g) + sind(a)*sind(b)*sind(g)))/2 - (cosd(a)*sind(g))/2 + (cosd(g)*sind(a)*sind(b))/2);
+y6 = dy - y0/2 + (3^(1/2)*x0)/2 + (dx/2 + (3^(1/2)*dy)/2)*((cosd(b)*sind(g))/2 - (3^(1/2)*cosd(b)*cosd(g))/2) - (dy/2 - (3^(1/2)*dx)/2)*((cosd(a)*cosd(g))/2 + (3^(1/2)*(cosd(a)*sind(g) - cosd(g)*sind(a)*sind(b)))/2 + (sind(a)*sind(b)*sind(g))/2);
 z6 = z0 + sind(b)*(dx/2 + (3^(1/2)*dy)/2) + cosd(b)*sind(a)*(dy/2 - (3^(1/2)*dx)/2);
 
 p = [x1, y1, z1;
