2ce4998ff3982dd3be9742325c2f2c9b7ab77b7f
Haussbrandt <haussbrandt@gmail.com>
2019-05-18 13:48:41 +0200

    function now returns both results

--- odwrotne.m
+++ odwrotne.m
@@ -1,4 +1,4 @@
-function [qa, qb] = odwrotne(l, p)
+function q = odwrotne(l, p)
 q11 = atan2(p(2), p(1)) - real(acos((l(1)^2 + (sqrt(p(1)^2 + p(2)^2))^2 - l(2)^2) / (2*l(1) * sqrt(p(1)^2 + p(2)^2))));
 q12 = atan2(p(2), p(1)) + real(acos((l(1)^2 + (sqrt(p(1)^2 + p(2)^2))^2 - l(2)^2) / (2*l(1) * sqrt(p(1)^2 + p(2)^2))));
 
@@ -7,4 +7,5 @@ q22 = -pi + real(acos((l(2)^2 + l(1)^2 - (sqrt(p(1)^2 + p(2)^2))^2) / (2 * l(1) 
 
 qa = [q11;q21];
 qb = [q12;q22];
+q = [qa,qb];
 end
